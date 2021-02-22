f725d8b2b8644e9f4e6aa8b05aea5abff157e0a0
ChuChuCode <zero789456321@gmail.com>
2021-02-22 18:06:18 +0800

    First_ver

---
+++ JenIng_plugin.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*- 
+import json
+#import os
+import subprocess
+from opencc import OpenCC
+from pathlib import Path
+
+# https://github.com/BYVoid/OpenCC
+converter = OpenCC('s2t')
+
+def check_0(str,n):
+    s  = ""
+    if len(str) < n:
+        for j in range( n - len(str) ):
+            s = s + "0"
+    s = s + str
+    return s
+
+try:
+    #file_path = os.getcwd()
+    
+    p = subprocess.Popen("cd", stdout=subprocess.PIPE, stderr=None, shell=True)
+    file_path = p.communicate()[0].decode("utf-8")[:-2]
+    #print(file_path)
+    #text = input('Press Enter')
+
+    #Read setting.dll
+    with open("setting.dll",'r') as setting :
+        for se in setting:
+            if 'App_path' in se:
+                app_path = se.split("=")[1].split("\n")[0]
+            elif 'export_path' in se:
+                ex_path = se.split("=")[1].split("\n")[0]
+    app_path = app_path + r"\User Data\Projects\com.lveditor.draft"
+    #os.chdir(r'{}'.format(app_path))
+
+    input_file = open(app_path + '\\'+'root_draft_meta_info.json')
+    json_array = json.load(input_file)
+
+    for i in json_array['all_draft_store']:
+        file_name =  i['draft_name']
+        path1 = app_path + '\\' +  i["draft_id"]
+        my_file = Path(path1+r'\draft.json')
+        if my_file.is_file():
+        
+            input_file = open (path1+r'\draft.json' ,encoding="utf-8")
+            lyrics = json.load(input_file)
+            time={}
+
+            for i in lyrics['materials']['texts']:
+                conv = converter.convert(i['content'])  # translate
+                time[i['id']] = [ conv ]
+                
+            for i in lyrics['tracks'][1]['segments']:
+                id = i['material_id']
+                sta = i['target_timerange']['start']//1000
+                dur = i['target_timerange']['duration']//1000
+                end = sta+ dur  
+                    
+                time[id].append(check_0(str(sta//3600//1000),2)+':'+check_0(str(sta//1000//60%60),2) +':' + check_0(str(sta//1000%60),2)+','+check_0(str(sta%1000),3))
+                time[id].append(check_0(str(end//3600//1000),2)+':'+check_0(str(end//1000//60%60),2) +':' + check_0(str(end//1000%60),2)+','+check_0(str(end%1000),3))
+
+            num = 1
+                
+            if ex_path == "" :
+                ex_path = file_path
+            with open(ex_path + '\\' + file_name + '.srt',"w",encoding="utf-8") as f:
+                for i in time:
+                    if num != 1:
+                        f.write("\n")
+                    f.write(str(num)+"\n")
+                    f.write(time[i][1] +' --> '+time[i][2]+"\n")
+                    f.write(time[i][0]+"\n")
+                    num = num + 1
+            print("Generate {}.srt".format(file_name))
+except Exception as e :
+    print(e)
